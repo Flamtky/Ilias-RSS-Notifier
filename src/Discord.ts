0279ff26b6d5580b422240f2b258de6aa8aa91d3
Flamtky <n.schumann@rz-online.de>
2022-11-04 13:15:34 +0100

    added new msg type forum

--- src/Discord.ts
+++ src/Discord.ts
@@ -1,25 +1,23 @@
-import axios from 'axios';
+import axios from "axios";
+import {IliasFeedItem} from "./IliasFeed";
 
 /**
  * It takes a string, formats it, and sends it to Discord
- * @param {string} embededMsg - The message you want to send to Discord.
- * @param {string} link - The link to change.
- * @param {string} timestamp - The timestamp of the message.
- * @returns {Promise<void>} A promise that resolves when the message is sent. It rejects if the message could not be sent.
+ * @param {item} item - IliasFeedItem
  */
-export const sendToDiscord = (msg: string, link: string, timestamp: string, _depth = 1) => {
-	const embededMsg = prepareDiscordMsg(msg, link, timestamp);
+export const sendToDiscord = (item: IliasFeedItem, _depth = 1) => {
+	const embededMsg = prepareDiscordMsg(item);
 	return new Promise<void>((resolve, reject) => {
 		axios.post(process.env.WEBHOOK_URL ?? '', embededMsg, {
 			headers: {
-				'Content-Type': 'application/json'
+				"Content-Type": "application/json",
 			}
 		}).catch((err) => {
 			if (err.response.status === 429 && _depth < 6) { // 429 = Too Many Requests | 5 retries
 				const timeout = (err.response.headers['retry-after'] ?? 1) * _depth;
 				console.warn(`Got 429 from Discord. Retrying in ${timeout}ms`);
 				setTimeout(() => {
-					sendToDiscord(msg, link, timestamp, _depth + 1).then(() => {
+					sendToDiscord(item).then(() => {
 						resolve();
 					}).catch((err) => {
 						reject(err);
@@ -31,45 +29,118 @@ export const sendToDiscord = (msg: string, link: string, timestamp: string, _dep
 		}).then((res) => {
 			resolve();
 		});
+    });
+};
+
+const prepareEmbededMsg = (title: string, text: string, link: string, color: number | string, timestamp: string,type: string) => {
+	const msgColor = 
+			typeof color === "number"
+			? color
+			: parseInt(color.replace("#", "0x") ?? "0x003c65");
+	const date = new Date(timestamp);
+	return JSON.stringify({
+		embeds: [
+			{
+				title: title,
+				description: text,
+				url: link,
+				color: msgColor,
+				author: {
+					name: process.env.MESSAGE_AUTHOR,
+					url: process.env.MESSAGE_AUTHOR_URL,
+					icon_url: process.env.MESSAGE_AUTHOR_ICON,
+				},
+				timestamp: timestamp,
+				footer: {
+					text: (process.env.MESSAGE_FOOTER ?? "#time").replace(
+						"#time",
+						date.toLocaleTimeString(undefined, {hour12: false})
+					),
+				},
+				thumbnail: {
+					url:
+						type === "newFile"
+							? process.env.MESSAGE_THUMBNAIL_NEWFILE
+							: process.env.MESSAGE_THUMBNAIL_FORUM
+							?? process.env.MESSAGE_THUMBNAIL,
+				},
+			},
+		],
 	});
+};
+
+/**
+ * Prepares a message for a new file.
+ * @param {RegExpExecArray} match - The regex match
+ * @param {string} link - The link to the file
+ * @param {string} timestamp - The timestamp of the message.
+ * @param {Date} date - Date - The date of the message
+ * @returns A JSON string.
+ */
+const prepareMsgNewFile = (match: RegExpExecArray, item: IliasFeedItem) => {
+	const link = item.link;
+	const timestamp = item.isoDate;
+	return prepareEmbededMsg(
+		match?.[1],
+		match?.[3].replace("Die Datei", match?.[2]),
+		link,
+		parseInt(
+			process.env.MESSAGE_COLOR_NEWFILE?.replace("#", "0x") ?? "0x003c65"
+		),
+		timestamp,
+		"newFile"
+	);
+};
+
+/**
+ * It takes an item from the RSS feed and returns a Discord embed message
+ * @param {IliasFeedItem} item - IliasFeedItem
+ * @returns A Discord embeded message
+ */
+const prepareMsgForum = (item: IliasFeedItem) => {
+	const link = item.link;
+	const timestamp = item.isoDate;
+	let content = item.content.replace(/<[^>]*>?/gm, ''); // remove html tags
+	content = content.replaceAll('\n', ''); // remove newlines
+	content = content.replaceAll('\r', ''); // remove carriage returns
+	return prepareEmbededMsg(
+		item.title,
+		content,
+		link,
+		parseInt(process.env.MESSAGE_COLOR_FORUM?.replace("#", "0x") ?? "0x003c65"),
+		timestamp,
+		"forum"
+	);
 };
 
 /**
  * It takes a string, checks if it matches a certain regex, and if it does, it returns a JSON string
  * with the matched groups as values.
- * @param {string} msg - The message that is being sent to Discord.
+ * @param {IliasFeedItem} item - IliasFeedItem
  * @returns {string} A stringified JSON object.
  */
-const prepareDiscordMsg = (msg: string, link: string, timestamp: string):string => {
-	const regex = /^\[(.+?)\] (.+): (.+)$/gm;
+const prepareDiscordMsg = (item: IliasFeedItem): string => {
+	const msgTypes = ["newFile", "forum"];
 
-	const match = regex.exec(msg);
-	if (!match)
-		return JSON.stringify({
-			content: msg
-		});
-	const date = new Date(timestamp);
-	const message = JSON.stringify({
-		"embeds": [
-		  {
-			"title": match?.[1],
-			"description": match?.[3].replace('Die Datei', match?.[2]),
-			"url": link,
-			"color": parseInt(process.env.MESSAGE_COLOR?.replace('#', "0x") ?? '0x003c65'),
-			"author": {
-				"name": process.env.MESSAGE_AUTHOR,
-				"url": process.env.MESSAGE_AUTHOR_URL,
-				"icon_url": process.env.MESSAGE_AUTHOR_ICON
-			},
-			"timestamp": timestamp,
-			"footer": {
-				"text": (process.env.MESSAGE_FOOTER ?? '#time').replace('#time', date.toLocaleTimeString(undefined, { hour12: false })),
-			},
-			"thumbnail": {
-				"url": process.env.MESSAGE_THUMBNAIL
+	const regexNewFile = /^\[(.+?)\] (.+): (.+)$/gm; // New File Posted [Course Name] [File Name]: [File Description]
+	const matchNewFile = regexNewFile.exec(item.title);
+
+	const currType = matchNewFile
+		? msgTypes[0]
+		: item.contentSnippet.length > 0
+		? msgTypes[1]
+		: null; // check if new file -> check if message available -> else null
+
+	switch (currType) {
+		case msgTypes[0]:
+			if (matchNewFile) {
+				return prepareMsgNewFile(matchNewFile, item);
 			}
-		  }
-		]
+		case msgTypes[1]:
+			return prepareMsgForum(item);
+	}
+
+	return JSON.stringify({
+		content: item.title,
 	});
-	return message;
-}
+};

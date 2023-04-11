ebd8629520c4f006069f25f8288305ce42479d71
Flamtky <n.schumann@rz-online.de>
2023-04-11 11:23:29 +0200

    changed logging to create files on first write

--- src/Logging.ts
+++ src/Logging.ts
@@ -6,26 +6,33 @@ const LOGFILE_PATH = DATA_PATH + LOGFILE_NAME;
 
 // Logging
 // Rename log.txt to {createdDate}.log
-if (fs.existsSync(LOGFILE_PATH)) {
-	const fileCreatedDate = fs.statSync(LOGFILE_PATH).birthtime;
-	// YYYY-MM-DD_HH-mm-ss
-	let newFileName = fileCreatedDate.toLocaleString('sv', { timeZoneName: 'short' })
-		.replace(/:/g, '-')
-		.replace(/ /g, '_')
-		.replace(/_(?!.*_.*).*$/, '');
-	newFileName += '.log';
-	fs.renameSync(LOGFILE_PATH, DATA_PATH + newFileName);
+let logFile:any = null;
+const createLogFile = () => {
+	if (fs.existsSync(LOGFILE_PATH)) {
+		const fileCreatedDate = fs.statSync(LOGFILE_PATH).birthtime;
+		// YYYY-MM-DD_HH-mm-ss
+		let newFileName = fileCreatedDate.toLocaleString('sv', { timeZoneName: 'short' })
+			.replace(/:/g, '-')
+			.replace(/ /g, '_')
+			.replace(/_(?!.*_.*).*$/, '');
+		newFileName += '.log';
+		fs.renameSync(LOGFILE_PATH, DATA_PATH + newFileName);
+	}
+	logFile = fs.createWriteStream(LOGFILE_PATH, { flags: 'a' });
 }
 
-const logFile = fs.createWriteStream(LOGFILE_PATH, { flags: 'a' });
 const logStdout = process.stdout;
 const errorStdout = process.stderr;
 
 console.warn = (...args: any[]) => {
-	logFile.write(new Date().toISOString() + ' [WARN] ' + args.join(' ') + ' ' + new Error().stack);
-	logStdout.write(new Date().toISOString() + ' [WARN] ' + args.join(' ') + '	' + new Error().stack);
+	if (logFile === null) createLogFile();
+	let str = new Date().toISOString() + ' [WARN] ' + args.join(' ') + '\n ' + new Error().stack + '\n '
+	logFile.write(str);
+	logStdout.write(str);
 };
 console.error = (...args: any[]) => {
-	logFile.write(new Date().toISOString() + ' [ERROR] ' + args.join(' ') + ' ' + new Error().stack);
-	errorStdout.write(new Date().toISOString() + ' [ERROR] ' + args.join(' ') + '	' + new Error().stack);
+	if (logFile === null) createLogFile();
+	let str = new Date().toISOString() + ' [ERROR] ' + args.join(' ') + '\n ' + new Error().stack + '\n '
+	logFile.write(str);
+	errorStdout.write(str);
 };
